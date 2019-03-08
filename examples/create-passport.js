d7908857d797e5f49a7fb7960a6554502b34290b
Gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2019-03-08 09:56:53 -0500

    Updated create passport example for create with NEO WIF option

--- examples/create-passport.js
+++ examples/create-passport.js
@@ -3,7 +3,13 @@ const _bridge = require("../src/index");
 
 async function Init() {
     let passportHelper = new _bridge.Passport();
+
+    //Create passport and generate NEO wallet
     let passport = await passportHelper.createPassport("12345");
+    console.log(JSON.stringify(passport));
+
+    //Create passport and use existing NEO wallet (WIF)
+    passport = await passportHelper.createPassport("12345","KyHPUiRAs9UnTUQMSiLRhLCAT31dDNyd4y9FchWJZK7w7gDL1iRf");
     console.log(JSON.stringify(passport));
 
     passportHelper.savePassportToFile(passport, './passport.json');
