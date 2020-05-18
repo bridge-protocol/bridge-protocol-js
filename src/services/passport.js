948fb7f1631c9baade3aa34e712039fdbaa57f9e
Gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2020-05-18 15:01:03 -0400

    Added passport publishing status endpoint handling Bump to 3.0.16

--- src/services/passport.js
+++ src/services/passport.js
@@ -8,6 +8,30 @@ class PassportApi {
         var res = await api.callApi("GET", passportId + "/details", null);
         return res.details;
     }
+
+    async createPassportPublish(passport, passphrase, network, address){
+        let obj = {
+            network,
+            address
+        };
+        var api = new _api.APIUtility(_apiBaseUrl, passport, passphrase);
+        let res = await api.callApi("POST", "publish/create", obj);
+
+        if(res.passportPublish)
+            return res.passportPublish;
+
+        return null;
+    }
+
+    async getPendingPassportPublishList(passport, passphrase){
+        var api = new _api.APIUtility(_apiBaseUrl, passport, passphrase);
+        let res = await api.callApi("GET", "publish/pending", null);
+        
+        if(res.passportPublish)
+            return res.passportPublish;
+
+        return null;
+    }
 };
 
 exports.PassportApi = new PassportApi;
